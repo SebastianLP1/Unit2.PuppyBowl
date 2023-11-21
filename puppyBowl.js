2c52357db56c05a82208827520a97779d5a1e1a9
SebastianLP1 <slugoparedes@gmail.com>
2023-11-20 23:25:03 -0800

    unable to get code to display id information

--- puppyBowl.js
+++ puppyBowl.js
@@ -1,9 +1,42 @@
 //2310-FSA-ET-WEB-FT-SF
-const input = document.querySelector(`input`);
-const form = document.querySelector(`form`);
-form.addEventListener(`submit`, (event) => {
-    event.preventDefault();
-    const numberInput = input.value
-    console.log(numberInput)
-    
-})
+
+const apiBaseUrl = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF`
+
+const state = {
+   allPlayers: []
+}
+
+const getAllPlayers = async () => {
+   const response = await fetch(`${apiBaseUrl}/players`);
+   const jsonResponse = await response.json();
+   state.allPlayers = jsonResponse.data.players;
+   renderAllPlayers();
+}
+
+const getPlayerDetails = async(id) => {
+const response =  await fetch(`${apiBaseUrl}/players/${id}`)
+const responseJson = await response.json();
+   const playerDetails = responseJson.data
+console.log(playerDetails)}
+
+const renderAllPlayers = () => {
+
+   playersNames = state.allPlayers.map((singlePlayers) => {
+      return `<li id = '${singlePlayers.id}'>${singlePlayers.name}</li>`
+   });
+   const ol = document.createElement(`ol`)
+
+   ol.innerHTML = playersNames.join(``);
+   const main = document.querySelector(`main`);
+   main.appendChild(ol);
+   //console.log(main)
+   const listItems = document.querySelectorAll(`li`);
+   listItems.forEach((playerListItem) => {
+      playerListItem.addEventListener(`click`, (event) => {
+         console.log(event.target.id)
+         
+      });
+   });
+}
+
+getAllPlayers();
